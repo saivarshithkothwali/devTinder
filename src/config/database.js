75292c235614c3609201dc1b2dbc85a93b35f240
Sreejakothwali <kothwalisaivarshith@gmail.com>
2025-03-18 13:07:08 +0530

    init-database

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose=require("mongoose");
+
+const connectDB=async()=>{
+  await mongoose.connect(
+    "mongodb+srv://namastedev:joD0C4osLbgcGzKr@namastenode.qyxpw.mongodb.net/devTinder"
+  )};
+
+  module.exports=connectDB;
+
+
+
