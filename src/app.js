634c51a947dd6218e872114fa1f79c1b6aab0e04
Sreejakothwali <sreejakothwali04@gmail.com>
2025-03-18 13:07:08 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,33 +1,38 @@
 const express = require("express");
+const connectDB=require("./config/database");
 const app = express();
+const User=require("./models/user");
 
-app.use("/",(err,req,res,next)=>{
-  if(err)
-  {
-    res.status(500).send("Something went wrong");
-  }
+app.post("/signup",async(req,res)=>{
+      const userObj={
+        firstName:"Varshith",
+        lastName:"Kothwali",
+        emailid:"varshith@gmail.com",
+        password:"vars@123",
+        age:"23",
+        gender:"male"
+      };
+      const user=new User(userObj);
+      
+      try
+      {
+        await user.save();
+        res.send("user Added Succesfully");
+      }
+      catch(err){
+        res.status(400).send("Error Saving the user"+err.message);
+      }
+      
 });
 
+connectDB()
+  .then(()=>{
+    console.log("Database connected succesfully");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777");
+    });
+  })
+  .catch((err)=>{
+    console.error("Database cannnot be connected");
+  });
 
-app.get("/getUserData",(req,res)=>{
-    
-  try{
-    throw new Error("asdfgh");
-    res.send("All Data Sent");
-  } 
-  catch(err){
-    res.status(500).send("Some Error Contact Support Team");
-  } 
-});
-
-app.use("/",(err,req,res,next)=>{
-  if(err)
-  {
-    res.status(500).send("Something went wrong");
-  }
-});
-
-  
-app.listen(7777, () => {
-  console.log("Server is successfully listening on port 7777");
-});
