9344e9b71a44886eb6e906746fb149d3750abdd6
Sreejakothwali <sreejakothwali04@gmail.com>
2025-03-27 13:09:06 +0530

    Exploring validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 const mongoose=require("mongoose");
+const validator=require("validator");
+
 const userSchema=new mongoose.Schema({
   firstName:{
     type:String,
@@ -16,10 +18,20 @@ const userSchema=new mongoose.Schema({
     required:true,
     unique:true,
     trim:true,
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("Invalid Email address " +value);
+      }
+    },
   },
   password:{
     type:String,
     required:true,
+    validate(value){
+      if(!validator.isStrongPassword(value)){
+        throw new Error("Enter a Strong Password " +value);
+      }
+    },
   },
   age:{
     type:Number,
@@ -37,6 +49,11 @@ const userSchema=new mongoose.Schema({
   photoUrl:{
     type:String,
     default:"https://static.vecteezy.com/system/resources/previews/045/944/199/non_2x/male-default-placeholder-avatar-profile-gray-picture-isolated-on-background-man-silhouette-picture-for-user-profile-in-social-media-forum-chat-greyscale-illustration-vector.jpg",
+    validate(value){
+      if(!validator.isURL(value)){
+        throw new Error("Invalid photo URL:"+value);
+      }
+    },
   },
   about:{
     type:String,
