40fd3cd13b919f8dfb28b1b01dfee10fd9ad5417
Sreejakothwali <sreejakothwali04@gmail.com>
2025-03-31 19:54:25 +0530

    Adding userAuth middleware for profile

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,41 @@
 const jwt=require("jsonwebtoken");
+const User=require("../models/user");
 
 const userAuth=async(req,res,next)=>{
-  //Read the token from request cookies
-  const cookies=req.cookies;
-  const {token}=cookies;
+  
+  try
+  {
+    //Read the token from request cookies
+    const cookies=req.cookies;
+    const {token}=cookies;
+    if(!token)
+    {
+      throw new Error("Token is not valid");
+    }
 
-  const decodedObj=await jwt.verify(token,"DEV@Tinder$790");
+    //Validate the token  
+    const decodedObj=await jwt.verify(token,"DEV@Tinder$790");
+    
+    //Extract the _id from decodedObj(This is object destructuring)
+    const {_id}=decodedObj;
+
+    //Find the user with that _id
+    const user=await User.findById(_id);
+    
+    if(!user)
+    {
+      throw new Error("User doesnot exist");
+    }
+    req.user=user;
+    next();
+    
+  }
+  catch(err)
+  {
+    res.status(400).send("ERROR: " +err.message);
+  }
 };
 
-module.exports={adminAuth,userAuth};
+module.exports={
+  userAuth,
+};
