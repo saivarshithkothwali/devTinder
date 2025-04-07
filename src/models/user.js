788cfd611256ca12e9b94b1edde85f1c6ca1d5b3
Sreejakothwali <sreejakothwali04@gmail.com>
2025-04-07 23:58:13 +0530

    Queries and indexes,connection request API

--- src/models/user.js
+++ src/models/user.js
@@ -42,11 +42,11 @@ const userSchema=new mongoose.Schema({
   },
   gender:{
     type:String,
-    validate(value){
-      if(!["male","female","others"].includes(value)){
-        throw new Error("Gender is not valid");
-      }
+    enum:{
+      values:["male","female","others"],
+      message:`{VALUE} is not a valid gender type`
     },
+    
   },
   photoUrl:{
     type:String,
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,13 +1,65 @@
 const express=require("express");
 const requestRouter=express.Router();
 const {userAuth}=require("../middlewares/auth");
+const ConnectionRequest=require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestRouter.get("/sendConnectionRequest",userAuth,async(req,res)=>{
-  const user=req.user;
-  //Sending a connection Request
-  console.log("Sending a connection Request");
 
-  res.send(user.firstName+ " sent the connection Request");
+requestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
+  try
+  {
+      const fromUserId=req.user._id;
+      const toUserId=req.params.toUserId;
+      const status=req.params.status;
+
+      //Validation for checking whether a user is sending a connection request to himself. 
+      // if(fromUserId.equals(toUserId)){
+      //   throw new Error("cannot send connection request to yourself");
+      // }
+
+      //Sending only the allowed status types(Validation3)
+      const allowedStatus=["ignored","interested"];
+      if(!allowedStatus.includes(status)){
+        return res.status(400).json({message:"Invalid Status Type: "+status});
+      }
+
+      //Can send the conenction request to only the users present in DB collection(Validation2) 
+      const toUser=await User.findById(toUserId);
+      if(!toUser){
+        return res.status(404).json({message: "User not Found"});
+      }
+
+      //If there is an existing ConnectionRequest(Validation1)
+      const existingConnectionRequest=await ConnectionRequest.findOne({
+        
+        
+        $or:[
+          {fromUserId,toUserId},
+          {fromUserId:toUserId,toUserId:fromUserId},
+        ],
+      });
+      if(existingConnectionRequest)
+      {
+        return res.status(400).send({message: "Connection Request already exists"});
+      }
+
+      //Creating a new connection request
+      const connectionRequest=new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data=await connectionRequest.save();
+
+      res.json({
+        message:req.user.firstName+ " is " +status+ " in " +toUser.firstName,
+        data,
+      });
+  }
+  catch(err){
+    res.status(400).send("ERROR: " +err.message);
+  }
 });
 
 module.exports=requestRouter; 
