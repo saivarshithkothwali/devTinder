5fe0c54b4578d1de616e9bbb96a9b6f0524d6e08
Sai Varshith Kothwali <saivarshithkothwali@gamil.com>
2025-05-20 17:11:43 +0530

    live chat using websockets

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const connectDB=require("./config/database");
 const app = express();
 const cookieParser=require("cookie-parser");
 const cors=require("cors");
+const http=require("http");
+
 require('dotenv').config();
+
+require("./utils/cronjob");
 
 app.use(cors({
   origin:"http://localhost:5173",
@@ -18,12 +22,17 @@ const authRouter=require("./routes/auth");
 const profileRouter=require("./routes/profile");
 const requestRouter=require("./routes/request");
 const userRouter=require("./routes/user");
+const initializeSocket=require("./utils/socket");
+
 
 
 app.use("/",authRouter);
 app.use("/",profileRouter);
 app.use("/",requestRouter);
 app.use("/",userRouter);
+
+const server=http.createServer(app);
+initializeSocket(server);
 
 
 
@@ -33,7 +42,7 @@ connectDB()
   .then(async()=>{
     console.log("Database connected succesfully");
 
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("Server is successfully listening on port 7777");
     });
   })
