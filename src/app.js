0455ea018a6c52d4e11dc35cf7893fc7033c63ac
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-05-23 15:02:29 +0530

    updating livechat 1

--- src/app.js
+++ src/app.js
@@ -9,10 +9,22 @@ require('dotenv').config();
 
 require("./utils/cronjob");
 
+const allowedOrigins = [
+  "http://localhost:5173",        // for local dev
+  "https://thedevconnect.in"     // for production
+];
+
 app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
