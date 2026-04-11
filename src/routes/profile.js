4ecb78e9b7d416b03f94c8971aa2dfbdd9d598da
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2026-04-11 22:40:23 +0530

    updated the profile view and edit API's

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,39 +1,63 @@
-const express=require("express");
-const profileRouter=express.Router();
-const {userAuth}=require("../middlewares/auth");
-const {validateEditProfileData}=require("../utils/validation");
+const express = require("express");
+const profileRouter = express.Router();
+const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
-profileRouter.get("/profile/view",userAuth,async(req,res)=>{
-  
-  try
-  {
-    const user= req.user;
-    res.send(user);
-  }
-  catch(err)
-  {
-    res.status(400).send("ERROR: "+err.message);
+const USER_SAFE_FIELDS = [
+  "firstName",
+  "lastName",
+  "photoUrl",
+  "skills",
+  "age",
+  "gender",
+  "about",
+];
+
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+
+    const safeUser = USER_SAFE_FIELDS.reduce((acc, field) => {
+      acc[field] = user[field];
+      return acc;
+    }, {});
+
+    res.status(200).json({
+      success: true,
+      data: safeUser,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    });
   }
 });
 
-profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
-    try{
-      if(!validateEditProfileData(req)){
-        throw new Error("Invalid Edit Request");
-      }
-      const loggedInUser=req.user;
-      
-      
-      Object.keys(req.body).forEach((key)=>(loggedInUser[key]=req.body[key]));
-      await loggedInUser.save();
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-      
+    if (!validateEditProfileData(req)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid fields in edit request",
+      });
+    }
 
-      res.send(`${loggedInUser.firstName} your Profile Updated Successfully`);
-    }
-    catch(err){
-      res.status(400).send("ERROR: " +err.message);
-    }
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+    await loggedInUser.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Profile updated successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to update profile",
+    });
+  }
 });
 
-module.exports=profileRouter;
+module.exports = profileRouter;
